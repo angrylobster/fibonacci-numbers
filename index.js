adcec2052bc2fc080e931748e768ba8aee04d90b
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-20 15:57:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ const fibonacci = (x, returnList) => {
   return numbers[x];
 };
 
-console.log(fibonacci(process.argv[2], process.argv[3]));
+//console.log(fibonacci(process.argv[2], process.argv[3]));
+
+function fib(n) {
+  if (n === 1 || n === 2) return 1;
+
+  return fib(n - 2) + fib(n - 1);
+}
+
+console.log(fib(process.argv[2]));
